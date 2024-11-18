f1d8267931813b90ebc0d459f95c1f9a48895ca8
Marlon Tomas Marzo Fernandez <marlonmarzo2@gmail.com>
2024-11-18 11:47:27 -0400

    Saludo entre paginas(3)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProductosComponent } from './paginas/productos/productos.component';
 import { ContactoComponent } from './paginas/contacto/contacto.component';
+import { CompraComponent } from './paginas/compra/compra.component';
 
 export const routes: Routes = [
     { path: 'productos',component: ProductosComponent},
-    { path: 'mi-contacto',component:ContactoComponent}
+    { path: 'mi-contacto',component:ContactoComponent},
+    { path: 'compra',component:CompraComponent}
+
 ];
     
